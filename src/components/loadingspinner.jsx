c7ffa68fd3517e04495faeefc7d66a3612cd0068
Zakaria Maarouf <zakaria@3d-curve.fr>
2025-10-17 15:00:58 +0100

    spinner

--- src/components/loadingspinner.jsx
+++ src/components/loadingspinner.jsx
@@ -1,18 +1,10 @@
 export const LoadingSpinner = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="30"
-      height="30"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="black" // Changed stroke to "black"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="animate-spin"
-    >
-      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-    </svg>
+    <div className="flex items-center justify-center p-4">
+      <div className="relative w-10 h-10">
+        <div className="absolute inset-0 rounded-full border-[3px] border-gray-200"></div>
+        <div className="absolute inset-0 rounded-full border-[3px] border-t-blue-500 border-r-transparent border-b-transparent border-l-transparent animate-spin"></div>
+      </div>
+    </div>
   );
-}
+};
